d65b55519c8bec15a7de88f8b3791e3a8b999f4b
Andrey1166 <aannddrreeyy266@gmail.com>
2025-12-10 01:18:50 +0200

    solution

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -4,35 +4,23 @@ import { Good } from '../types/Good';
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
 export function getAll(): Promise<Good[]> {
-  return fetch(API_URL)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Error!!!');
-      }
+  return fetch(API_URL).then(response => {
+    if (!response.ok) {
+      throw new Error('Error!!!');
+    }
 
-      return response.json();
-    })
-    .catch(() => {
-      throw new Error('Something gone wrong');
-    });
+    return response.json();
+  });
 }
 
 export const get5First = () => {
-  return getAll()
-    .then(goods =>
-      [...goods]
-        .sort((item1, item2) => item1.name.localeCompare(item2.name))
-        .slice(0, 5),
-    )
-    .catch(() => {
-      throw new Error('Something gone wrong');
-    });
+  return getAll().then(goods =>
+    [...goods]
+      .sort((item1, item2) => item1.name.localeCompare(item2.name))
+      .slice(0, 5),
+  );
 };
 
 export const getRedGoods = () => {
-  return getAll()
-    .then(goods => goods.filter(good => good.color === 'red'))
-    .catch(() => {
-      throw new Error('Something gone wrong');
-    });
+  return getAll().then(goods => goods.filter(good => good.color === 'red'));
 };
